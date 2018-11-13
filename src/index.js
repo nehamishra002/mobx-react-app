9c47cbd183440f09e6cdc91efc629db65a13d16c
rashmiap <rashmiap.10@gmail.com>
2018-11-13 16:14:29 +0530

    added observable created observable store to save state
    passing it as props to appState

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { observable } from 'mobx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const appState = observable({
+  count: 10,
+})
+appState.handleInc = () => {
+  appState.count++;
+}
+appState.handleDec = () => {
+  appState.count--;
+}
+ReactDOM.render(<App store={appState}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
