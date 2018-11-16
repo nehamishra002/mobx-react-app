b09c73d2b3cc7a35819403f0f2b41e25d60eb691
rashmiap <rashmiap.10@gmail.com>
2018-11-16 12:21:49 +0530

    a simple counter component created injecting with CounterStore

---
+++ src/Components/Counter.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+import { inject, observer } from 'mobx-react';
+import DevTools from 'mobx-react-devtools';
+
+@inject('CounterStore')
+@observer class Counter extends Component {
+  handleDec = () =>{
+    this.props.CounterStore.handleDec();
+  }
+  handleInc = () =>{
+    this.props.CounterStore.handleInc();
+  }
+  render() {
+    return (
+      <div className="App">
+        <DevTools />
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <h2>Counter</h2>
+        <div>
+          <p>Count: {this.props.CounterStore.count} </p>
+          <button onClick={this.handleDec}> - </button>
+          <button onClick={this.handleInc}> + </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
