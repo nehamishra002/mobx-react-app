835ab200927c237f1f9b0c5d57ac96ae426d34f9
rashmiap <rashmiap.10@gmail.com>
2018-11-20 17:24:40 +0530

    filter added clear all create new todo basic setup up and running

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -3,15 +3,45 @@ import { inject, observer } from 'mobx-react';
 
 @inject('TodoStore')
 @observer class Todo extends Component {
+
+  createNew(e){
+    if(e.which === 13){
+      this.props.TodoStore.createTodo(e.target.value);
+      e.target.value = "";
+    }
+  }
+  handleFilter(e){
+    this.props.TodoStore.filter = e.target.value;
+  }
+  toggleComplete(todo){
+    console.log(todo);
+    todo.completed = !todo.completed;
+  }
   render() {
+    const {todos, filter, filteredTodo, clearCompleted} = this.props.TodoStore;
+
+    const renderTodo = filteredTodo.map(todo => (
+      <li key={todo.id}>
+      <input type="checkbox" onChange={this.toggleComplete(this, todo)} value={todo.completed} />
+      {todo.value}
+      </li>
+    ))
+
     return (
       <div className="Todo">
         <p>Todo</p>
+        <div>
+          <p>Create new todo </p>
+          <input className="create" onKeyPress={this.createNew.bind(this)} />
+        </div>
+        <div>
+          <p>Search todo {filter} </p>
+          <input className="filter" value={filter} onChange={this.handleFilter.bind(this)} />
+        </div>
         <ol>
-        {this.props.TodoStore.todos.map(function(name, index){
-          return <li key={ index }>{name}</li>;
-        })}
+          {renderTodo}
         </ol>
+        <a href="#" onClick={clearCompleted}> Clear Completed tasks </a>
       </div>
     );
   }
